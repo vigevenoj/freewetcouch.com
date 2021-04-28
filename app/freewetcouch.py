53161615b3052ba1bd255483d975af10e492add4
Jacob Vigeveno <304282+vigevenoj@users.noreply.github.com>
2021-04-27 19:06:41 -0700

    minimal python flask app

---
+++ app/freewetcouch.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for, make_response
+import random, os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/style.css')
+def css():
+    response = make_response(render_template("style.css", couch_file=couch()))
+    response.headers['Content-Type'] = 'text/css'
+    return response
+
+
+def couch():
+    path = './app/static'
+    random_couch = random.choice([x for x in os.listdir(path)
+                                  if os.path.isfile(os.path.join(path, x))])
+    return url_for('static', filename=random_couch)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
